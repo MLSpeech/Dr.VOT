c03dfe22dcd4e8fbd585a394498ba4e233657ca9
yosishrem <yosishrem1@gmail.com>
2019-11-10 14:49:23 +0200

    dr.vot

--- helpers/check_req.py
+++ helpers/check_req.py
@@ -5,20 +5,23 @@ try:
 
     # praat
     if os.popen('uname -a').read().lower().__contains__("darwin"):
-        assert os.path.exists("/Applications/Praat.app/Contents/MacOS/Praat"), \
-            f"Couldn't Find praat at : '/Applications/Praat.app/Contents/MacOS/Praat'"
+        if not os.path.exists("/Applications/Praat.app"):
+            raise SystemExit("Couldn't Find praat at : '/Applications/Praat.app', please make sure praat exists at this location")
+        if not os.path.exists("/Applications/Praat.app/Contents/MacOS/Praat"):
+            raise SystemExit("praat.app is corrupted, please re-install")
+
     elif os.popen('uname -a').read().lower().__contains__("linux"):
-        assert os.path.exists(os.path.join(os.getcwd(), "linux_praaat")), \
-            f"Couldn't find Praat at :{os.path.join(os.getcwd(), 'linux_praat')}"
+        if not os.path.exists(os.path.join(os.getcwd(), "linux_praaat")):
+            raise SystemExit(f"Couldn't find Praat at :{os.path.join(os.getcwd(), 'linux_praat')}")
     else:
-        raise Exception("Unsupported Operating System, support only MacOS & Linux")
+        raise SystemExit("Unsupported Operating System, support only MacOS & Linux")
     # sox
     if not os.popen('sox --version').read().lower().__contains__("sox"):
-        raise Exception(f"Couldn't find SoX. Check the tutorial for links")
+        raise SystemExit(f"Couldn't find SoX. Check the tutorial for links")
 
     # pipenv
     if not os.popen('pipenv --version').read().lower().__contains__("pipenv"):
-        raise Exception(f"Couldn't find pipenv. Check the tutorial for links")
+        raise SystemExit(f"Couldn't find pipenv. Check the tutorial for links")
 
 except Exception as e:
     print(f"Error: {e}")
